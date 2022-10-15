cb17f78b1f379782bf52488a757a4ace70207c0c
VikkramStark <vikkram26stark@gmail.com>
2022-10-15 15:39:34 +0530

    Nearly-over

--- pages/5_📈_Analytics.py
+++ pages/5_📈_Analytics.py
@@ -1,6 +1,10 @@
 import streamlit as st 
+import pandas as pd
+import plotly_express as px  
 import requests 
 import json  
+
+st.set_page_config(layout = 'wide') 
 
 
 key = "4d61f4c5-3032-4d63-bce5-c8bcfef4ba50" 
@@ -12,12 +16,60 @@ headers = {
 }
 
 st.sidebar.write("Select Category: ") 
-category = st.sidebar.selectbox('',["Maps","Trending Gainers&Losers", "Listings Latest"])  
+category = st.sidebar.selectbox('Filter : ',["Analysis Graph","Maps"])  
+
+
+if category == "Analysis Graph":
+    dataset = st.sidebar.selectbox("Select Data : ", ["Top NFTs","History Sales"]) 
+
+    if dataset == "Top NFTs":
+        top_collection = pd.read_csv("./Dataset/NFT_Top_Collections.csv") 
+        #st.write(top_collection)
+        values = st.sidebar.multiselect("Name: ",options = top_collection['Name'].unique()) 
+
+        if len(values) == 0:
+            #floor_price = px.histogram(top_collection,x=top_collection['Name'],y=top_collection['Floor_Price']) 
+            reference = st.sidebar.selectbox('Select Data Reference:',['Floor_Price','Average_Price','Volume','Sales','Owners'])  
+            for i in range(0,600,100):
+                 st.bar_chart(top_collection[i:i+100],x='Name',y=reference,use_container_width=True) 
+
+            #st.plotly_chart(floor_price) 
+            
+        else:
+            st.write("# :bar_chart: Analytics Dashboard")  
+            for value in values:
+                st.markdown("---") 
+                collection_selection = top_collection.query(f'Name == @value')  
+                st.markdown(f"## {value}")   
+                # avg_sales = px.bar(collection_selection,collection_selection['Market_Cap']) 
+                # avg_sales.add_bar(collection_selection,)  
+                # st.plotly_chart(avg_sales)  
+                # st.write(collection_selection)
+                analysis = px.histogram(collection_selection,x=collection_selection['Name'],y = [collection_selection['Volume'],collection_selection['Floor_Price']])   
+                st.bar_chart(collection_selection,x='Name',y=['Volume','Floor_Price','Sales'])             
+                st.plotly_chart(analysis)  
+
+
+    if dataset == "History Sales":
+        history_sales = pd.read_csv("./Dataset/NFT_Sales_History.csv") 
+        #st.dataframe(history_sales)   
+        st.markdown("# Historical Sales Analysis") 
+        left_column,right_column = st.columns(2)  
+        sales_hist = px.histogram(history_sales,x=history_sales['Date'],y=history_sales['AverageUSD_cum'])
+        number_of_sales = px.histogram(history_sales,history_sales['Number_of_Sales']) 
+
+        with left_column: 
+            st.plotly_chart(sales_hist)
+
+        with right_column:
+            st.plotly_chart(number_of_sales) 
+  
+
 
 
 def format_map(row):
     res = st.markdown(f"""
-        <div class = 'box' style = "display:flex; justify-content:space-around; align-items:center; margin:50px; border:2px solid white; border-radius:20px; padding:20px;">  
+        <div class = 'box' style = "display:flex; justify-content:space-around; align-items:center; margin:20px; border:2px solid white; border-radius:20px; padding:20px;">  
             <span>{row['id']}</span><span>{row["name"]}</span><span>{row['sign']}</span><span>{row['symbol']}</span> 
         </div> 
     """,unsafe_allow_html=True) 
@@ -30,7 +82,7 @@ if category == "Maps":
     data = response['data']
 
     st.markdown(f"""
-        <div class = 'box' style = "display:flex; justify-content:space-around; align-items:center; margin:50px; border-bottom:2px solid white; border-radius:0px; padding:20px;">  
+        <div class = 'box' style = "display:flex; justify-content:space-around; align-items:center;margin:20px; border-bottom:2px solid white; border-radius:0px; padding:20px;">  
             <span style = "font-weight:1000; font-size:30px;">ID</span><span style = "font-weight:1000; font-size:30px;">Name</span><span style = "font-weight:1000; font-size:30px;">SIGN</span><span style = "font-weight:1000; font-size:30px;">SYMBOL</span> 
         </div> 
     """,unsafe_allow_html=True) 
@@ -42,13 +94,25 @@ if category == "Maps":
         except:
             pass   
 
-if category == "Trending Gainers&Losers":
-    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/trending/gainers-losers" 
 
-if category == "Listings Latest":
-    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"  
-    r =  requests.get(url,headers = headers) 
-    st.write(r.json()) 
+
+
+# def format_latest(row):
+#     res = st.markdown("""
+#         <div class = 'container' style = "display:flex; ">
+            
+#         </div> 
+#     """,unsafe_allow_html=True)
+
+# if category == "Listings Latest":
+#     url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"  
+#     r =  requests.get(url,headers = headers) 
+#     response = r.json()
+#     st.write(r.json()) 
+#     data = response['data'] 
+#     for row in data:
+#         res = format_latest(row)
+#         st.write(res) 
 
 #currency = st.sidebar.selectbox("Conversion Currency: ", ['USD','INR']) 
 
